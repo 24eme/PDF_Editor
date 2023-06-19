5ef4cfb8dc9170817943e15199b2aba61be18376
Amalhamdi007 <amal@24eme.fr>
2023-06-19 16:03:46 +0200

    add possibility to extract many pages

--- page_extractor/page_extractor.py
+++ page_extractor/page_extractor.py
@@ -3,15 +3,25 @@ from pypdf import PdfReader, PdfWriter
 reader = PdfReader("../PDF_pool/chapitre2.pdf")
 number_of_pages = len(reader.pages)
 print(number_of_pages)
+result = []
 numero = -1
-while (numero<0 or numero>=number_of_pages):
-    print("Donner un num valide!")
-    numero = int(input("Donner moi la page que tu veux : "))
+response = 'y'
+while response == 'y' or response =='Y' :
+    while (numero<0 or numero>=number_of_pages):
+        print("Donner un num valide!")
+        numero = int(input("Donner moi la page que tu veux : "))
+    result.append(numero)
+    numero = -1
+    response = input("Voulez vous ajouter ajouter une autre page (repondre par y ou n ? ")
 
-page = reader.pages[numero]
 
 writer = PdfWriter()
-writer.add_page(page)
+for e in result:
+    page = reader.pages[e]
+    writer.add_page(page)
+
+
+
 with open("../PDF_pool/MyPDF.pdf", "wb") as fp:
     writer.write(fp)
 
