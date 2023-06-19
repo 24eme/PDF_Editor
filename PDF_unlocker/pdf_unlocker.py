e7a9979b367c8cfe0a7c114781a789b8435c4cd6
tale-fau <tanguy@24eme.fr>
2023-06-19 16:44:19 +0200

    add: pdf unlocker

--- PDF_unlocker/pdf_unlocker.py
+++ PDF_unlocker/pdf_unlocker.py
@@ -1,17 +1,37 @@
 import sys
+import os
 from PyPDF2 import PdfReader, PdfWriter
 
-reader = PdfReader("../PDF_pool/locked.pdf")
-if reader.is_encrypted:
-    writer = PdfWriter()
+
+def unlock_pdf_file(input_path, password):
+    reader = PdfReader(input_path)
 
     if reader.is_encrypted:
-        reader.decrypt("")
+        writer = PdfWriter()
+        if reader.decrypt(password):
+            # Add all pages to the writer
+            for page in reader.pages:
+                writer.add_page(page)
+            # Save the new PDF to a file
+            decrypted_file_name = "decryted_" + os.path.basename(input_path)
+            with open(decrypted_file_name, "wb") as f:
+                writer.write(f)
+        else:
+            print("Wrong password.")
+            exit()
+    else:
+        print("Your file is not encrypted.")
+        exit()
 
-    # Add all pages to the writer
-    for page in reader.pages:
-        writer.add_page(page)
+    print("Your file is unlocked.")
 
-    # Save the new PDF to a file
-    with open("decrypted-pdf.pdf", "wb") as f:
-        writer.write(f)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print(
+            "Veuillez spécifier les noms du fichier d'entrée PDF et du fichier de sortie. Utilisation : python3 "
+            "pdf_unlocker.py [input file] [password]")
+    else:
+        input_file = sys.argv[1]
+        password = sys.argv[2]
+        unlock_pdf_file(input_file, password)
