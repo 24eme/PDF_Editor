28843c6c11cb8359ed08a2d61f52cba7a27e1dc9
Amalhamdi007 <amal@24eme.fr>
2023-06-19 18:17:37 +0200

    fix create form

--- create_forms/create_forms.py
+++ create_forms/create_forms.py
@@ -7,32 +7,34 @@ def create_form():
     c = canvas.Canvas("../PDF_pool/"+'Amal Hamdi Convention de stage.pdf')
 
     c.setFont("Courier", 20)
-    c.drawCentredString(300, 700, 'Convention de stage')
+
+    c.drawCentredString(300, 750, 'Convention de stage')
     c.setFont("Courier", 14)
     form = c.acroForm
 
+    c.drawCentredString(100, 700, " ENTREPRISE D'ACCUEIL : ")
     c.drawString(10, 650, 'Nom:')
     form.textfield(name='fname', tooltip='Nom',
-                   x=110, y=635, borderStyle='inset',
+                   x=120, y=635, borderStyle='inset',
                    borderColor=magenta, fillColor=pink,
                    width=300,
                    textColor=blue, forceBorder=True)
 
-    c.drawString(10, 600, 'prénom:')
+    c.drawString(10, 600, 'représentée par:')
     form.textfield(name='lname', tooltip='prénom',
-                   x=110, y=585, borderStyle='inset',
+                   x=150, y=585, borderStyle='inset',
                    borderColor=green, fillColor=magenta,
                    width=300,
                    textColor=blue, forceBorder=True)
 
     c.drawString(10, 550, 'Addresse:')
     form.textfield(name='address', tooltip='Address',
-                   x=110, y=535, borderStyle='inset',
+                   x=120, y=535, borderStyle='inset',
                    width=400, forceBorder=True)
 
     c.drawString(10, 500, 'Ville:')
     form.textfield(name='city', tooltip='City',
-                   x=110, y=485, borderStyle='inset',
+                   x=120, y=485, borderStyle='inset',
                    forceBorder=True)
 
     c.drawString(250, 500, 'Région:')
@@ -42,12 +44,12 @@ def create_form():
 
     c.drawString(10, 450, 'Code Postale:')
     form.textfield(name='zip_code', tooltip='Zip Code',
-                   x=110, y=435, borderStyle='inset',
+                   x=120, y=435, borderStyle='inset',
                    forceBorder=True)
 
-    c.drawString(10, 400, 'Entreprise:')
+    c.drawString(10, 400, 'N° SIRET:')
     form.textfield(name='entreprise', tooltip='entreprise',
-                   x=110, y=385, borderStyle='inset',
+                   x=120, y=385, borderStyle='inset',
                    borderColor=green, fillColor=green,
                    width=300,
                    textColor=blue, forceBorder=True)
